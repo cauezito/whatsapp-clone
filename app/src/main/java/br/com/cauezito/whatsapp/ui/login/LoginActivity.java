39056eb8edea3511b7df3ac947ff65c3e19695a5
Cauê Santos <caah.sfc@live.com>
2020-11-07 15:07:44 -0300

    style: layout register screen

--- app/src/main/java/br/com/cauezito/whatsapp/ui/login/LoginActivity.java
+++ app/src/main/java/br/com/cauezito/whatsapp/ui/login/LoginActivity.java
@@ -1,27 +1,15 @@
 package br.com.cauezito.whatsapp.ui.login;
 
-import android.app.Activity;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 
 import br.com.cauezito.whatsapp.R;
 
@@ -47,10 +35,10 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         register.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent();
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
     }
---
+++ app/src/main/java/br/com/cauezito/whatsapp/ui/login/RegisterActivity.java
@@ -0,0 +1,16 @@
+package br.com.cauezito.whatsapp.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.cauezito.whatsapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
