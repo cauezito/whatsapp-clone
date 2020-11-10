5e29c3aa7ec68ff0f32d892bc45f97c7af1f7e1e
Cauê Santos <caah.sfc@live.com>
2020-11-10 17:33:16 -0300

    feat: registering user

--- app/src/main/java/br/com/cauezito/whatsapp/activity/RegisterActivity.java
+++ app/src/main/java/br/com/cauezito/whatsapp/activity/RegisterActivity.java
@@ -1,5 +1,6 @@
 package br.com.cauezito.whatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,16 +10,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import br.com.cauezito.whatsapp.R;
+import br.com.cauezito.whatsapp.config.FirebaseConfig;
+import br.com.cauezito.whatsapp.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView tvSignIn, tvEmail, tvPassword, tvName;
     private Button btRegister;
-
     private FirebaseAuth auth;
 
     @Override
@@ -32,45 +37,74 @@ public class RegisterActivity extends AppCompatActivity {
         tvName = findViewById(R.id.tvName);
         btRegister = findViewById(R.id.btRegister);
 
-        auth = FirebaseAuth.getInstance();
-
-      /*  btRegister.setOnClickListener(new View.OnClickListener() {
+        btRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = tvName.toString();
-                String email = tvEmail.toString();
-                String password = tvPassword.toString();
+                User user = new User();
+                user.setName(tvName.getText().toString());
+                user.setEmail(tvEmail.getText().toString());
+                user.setPassword(tvPassword.getText().toString());
 
-               if(validateData(name, email, password)) {
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
-                            new OnCompleteListener(AuthResult) task) {}
-                }
+                validateData(user);
             }
-        });*/
+        });
 
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-    public void signIn(View view){
-        finish();
+    private void addUser(User user) {
+        auth = FirebaseConfig.getAuth();
+        auth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User successfully registered!", Toast.LENGTH_SHORT).show();
+                            clearFields();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, showError(task), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
-    private boolean validateData(String tvName, String tvEmail, String tvPassword){
-        boolean validate = false;
+    private void clearFields() {
+        tvName.setText("");
+        tvEmail.setText("");
+        tvPassword.setText("");
+    }
 
-        if(!tvName.isEmpty() && !tvEmail.isEmpty() && !tvPassword.isEmpty()){
-            validate = true;
-        } else {
-            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+    private String showError(Task<AuthResult> task){
+        String exception = "";
+
+        try{
+            throw task.getException();
+        } catch (FirebaseAuthWeakPasswordException e) {
+            exception = "Enter a stronger password!";
+        } catch (FirebaseAuthInvalidCredentialsException e) {
+            exception = "Enter a valid email address";
+        } catch (FirebaseAuthUserCollisionException e){
+            exception = "This account already exists";
+        } catch (Exception e) {
+            exception = "Oh! Could not register user";
+            e.printStackTrace();
         }
 
-        return validate;
+        return exception;
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = auth.getCurrentUser();
-        //updateUI(currentUser);
+    
+    private void validateData(User user){
+        if(!user.getName().isEmpty() && !user.getEmail().isEmpty() && !user.getPassword().isEmpty()){
+           //validate
+            addUser(user);
+        } else {
+            Toast.makeText(this, "Pls, fill in all fields!", Toast.LENGTH_LONG).show();
+        }
     }
 }
