48334ab507b05ff8d719851c85a20de8046ecbcd
Cauê Santos <caah.sfc@live.com>
2020-11-11 17:25:18 -0300

    style: adding toolbar

--- app/src/main/java/br/com/cauezito/whatsapp/activity/HomeActivity.java
+++ app/src/main/java/br/com/cauezito/whatsapp/activity/HomeActivity.java
@@ -3,14 +3,20 @@ package br.com.cauezito.whatsapp.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import br.com.cauezito.whatsapp.R;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setSubtitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+
     }
 }
